78985bbefeb44e0a08403517527793a8223f8722
Nitin Budania <109571483+Nitin1613@users.noreply.github.com>
2026-05-09 22:30:38 +0530

    Update 2. Loading Local AI Models.py

--- 2. Loading Local AI Models.py
+++ 2. Loading Local AI Models.py
@@ -1,21 +1,23 @@
-print("Loading local AI models... (This may take a moment and requires internet on the FIRST run)")
+# LOAD MODELS
+@st.cache_resource
+def load_models():
 
-# 1. Load Local Embedding Model
-# all-MiniLM-L6-v2 is ultra-fast and uses ~100MB of RAM
-print("[1/3] Loading Embedder...")
-embedder = SentenceTransformer("all-MiniLM-L6-v2")
+    embedder = SentenceTransformer(
+        "nomic-ai/nomic-embed-text-v1.5",
+        trust_remote_code=True
+    )
 
-# 2. Load Local LLM (Qwen2.5-0.5B-Instruct)
-# Ultra-lightweight LLM,runs well on edge CPU
-print("[2/3] Loading LLM...")
-llm_pipeline = pipeline(
-    "text-generation",
-    model="Qwen/Qwen2.5-0.5B-Instruct",
-    device_map="auto" #auto-select GPU if available, otherwise runs on CPU
-)
+    llm_pipeline = pipeline(
+        "text-generation",
+        model="Qwen/Qwen2.5-0.5B-Instruct",
+        device_map="auto"
+    )
 
-# 3. Load Local ASR (Whisper Base)
-print("[3/3] Loading Whisper Voice Model")
-asr_model = whisper.load_model("base") # ~140MB model
+    asr_model = whisper.load_model("base")
 
-print("All models loaded successfully!\n")
+    return embedder, llm_pipeline, asr_model
+
+with st.spinner("Loading Models..."):
+    embedder, llm_pipeline, asr_model = load_models()
+
+st.success("Models Loaded Successfully")
