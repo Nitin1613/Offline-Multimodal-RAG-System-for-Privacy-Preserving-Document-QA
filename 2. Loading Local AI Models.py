f251250984a4228e63dbbb98086fe84109eca1f3
Nitin Budania <109571483+Nitin1613@users.noreply.github.com>
2026-05-09 22:33:43 +0530

    Update 2. Loading Local AI Models.py

--- 2. Loading Local AI Models.py
+++ 2. Loading Local AI Models.py
@@ -1,23 +1,27 @@
-# LOAD MODELS
 @st.cache_resource
 def load_models():
 
+    # Embedding Model
     embedder = SentenceTransformer(
         "nomic-ai/nomic-embed-text-v1.5",
         trust_remote_code=True
     )
 
+    # Local LLM
     llm_pipeline = pipeline(
         "text-generation",
         model="Qwen/Qwen2.5-0.5B-Instruct",
         device_map="auto"
     )
 
+    # Whisper ASR
     asr_model = whisper.load_model("base")
 
     return embedder, llm_pipeline, asr_model
 
-with st.spinner("Loading Models..."):
+
+with st.spinner("Loading AI Models..."):
+
     embedder, llm_pipeline, asr_model = load_models()
 
 st.success("Models Loaded Successfully")
