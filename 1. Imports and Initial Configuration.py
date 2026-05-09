c48e86a07bde1c8736057cff1f4105f843725e93
Nitin Budania <109571483+Nitin1613@users.noreply.github.com>
2026-05-09 22:33:10 +0530

    Update 1. Imports and Initial Configuration.py

--- 1. Imports and Initial Configuration.py
+++ 1. Imports and Initial Configuration.py
@@ -2,19 +2,29 @@ import os
 import glob
 import warnings
 import tempfile
+
 import streamlit as st
-import speech_recognition as sr
 import whisper
 import PyPDF2
 
 from sentence_transformers import SentenceTransformer, util
 from transformers import pipeline
+from streamlit_mic_recorder import mic_recorder
 
-# CONFIG
+# CONFIGURATION
 
 PDF_DIRECTORY = "my_pdfs"
+
 warnings.filterwarnings("ignore")
 
-st.set_page_config(page_title="Offline Edge RAG", layout="wide")
+st.set_page_config(
+    page_title="Offline Edge RAG",
+    layout="wide"
+)
 
 st.title("📚 Offline Edge RAG System")
+
+# SESSION STATE
+
+if "voice_query" not in st.session_state:
+    st.session_state.voice_query = ""
